cd4404fc142e5eaadaada9ed0f385ee90589746d
sachinabs <sachinabs.js@gmail.com>
2022-12-20 10:54:45 +0530

    learning: Button events, passing events in props

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -1,42 +1,21 @@
 
 import './App.css';
-import { Greet } from './components/Greet';
-import { Heading } from './components/Heading';
-import { Oscer } from './components/Oscer';
-import { Person } from './components/Person';
-import { PersonList } from './components/PersonList';
-import { Status } from './components/Status';
+import { Button } from './components/Button';
+import { InputBox } from './components/InputBox';
+
 
 function App() {
 
-  const personName = {
-    "first_name": "sachin",
-    "last_name": "abs"
-  }
-
-  const nameList = [{
-    "first_name": "sachin",
-    "last_name": "abs"
-  }, {
-    "first_name": "margo",
-    "last_name": "jseo"
-  }, {
-    "first_name": "regin",
-    "last_name": "mie"
-  }]
-
   return (
     <div className="App">
-      <Greet name="sachin abs" messageCount={20} isLogedIN={true} />
-      <Person name={personName} />
-      <PersonList name={nameList} />
-      <Status status='error' />
-      <Heading>
-        Placeholder text
-      </Heading>
-      <Oscer>
-        <Heading>Place Holder</Heading>
-      </Oscer>
+      <InputBox  handle_click={(event) => {
+        console.log(event.target.value);
+
+      }} />
+      <Button handle_click={(event, id) => {
+        console.log("works", event, id);
+        
+      }} />
     </div>
   );
 }
